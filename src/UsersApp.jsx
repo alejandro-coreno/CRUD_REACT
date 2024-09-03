46f7fe18533914cf48160e11d14e1c05e4f696fc
josue <alejandro@MacBook-Air-de-josue.local>
2024-09-02 22:10:24 -0600

    Se agrega componentes

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,10 +1,35 @@
+import { UserForm } from "./components/UserForm";
+import { UsersList } from "./components/UsersList";
 
 const UsersApp = () => {
+    // Arreglo de usuarios
+
+    const initialUsers = [
+        {
+            id: 1,
+            username: 'Alejandro',
+            password: '12345',
+            email: 'correo@correo.com'
+        }
+    ]
+
     return (
         <div className="container my-4">
             <h1>User App</h1>
+
+            {/* Una fila para los dos componentes */}
+            <div className="row">
+
+                <div className="col">
+                    <UserForm />
+                </div>
+
+                <div className="col">
+                    <UsersList/>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default UsersApp;
---
+++ src/components/UsersList.jsx
@@ -0,0 +1,8 @@
+export const UsersList = () => {
+    return (
+        <>
+            <p>Listao de Usuarios</p>
+            <table></table>
+        </>
+    )
+}
