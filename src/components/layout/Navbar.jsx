09c018c7c9184f6a58cc694d3a3a3c4d4b3feeae
josue <alejandro@MacBook-Air-de-josue.local>
2024-09-10 22:04:00 -0600

    Se agrega componente Navlink en Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,4 @@
-
-
+import { NavLink } from "react-router-dom";
 
 // pasamos por props el objeto login y la funcion de salir 
 export const Navbar = ({ login, handlerLogout }) => {
@@ -11,7 +10,16 @@ export const Navbar = ({ login, handlerLogout }) => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/users">Usuarios</NavLink>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavLogout">
                     <span className="nav-item nav-link text-primary mx-3">{login.user?.username}</span>
                     <button 
                         type="button" 
